95dc639087ad5a8712a5864e9182029a06cc2970
yrx <rongxingyang@outlook.com>
2022-09-10 14:36:09 +0800

    login logout

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,9 +7,57 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+// import $ from 'jquery'
+
 export default {
   components:{
     NavBar
+  },
+  setup(){
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: "yzd",
+    //     password: "yzd",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0OWExZGFhMjZhNmI0MjMwYjNhZjlhNDJmMmExNjc4MyIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY2MjczNzE3OCwiZXhwIjoxNjYzOTQ2Nzc4fQ.4kqQuUfTNLzoMHJ5Hc6CBlck19vax5rXqEBcoDipM7w"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   },
+    // })
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "yzdd",
+    //     password: "111",
+    //     confirmedPassword: "111",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   },
+    // })
   }
 }
 </script>
--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
-document.title = "蛇战群雄";
+createApp(App).use(store).use(router).mount('#app')
